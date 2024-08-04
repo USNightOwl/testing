6819365d7fb495255dbc3bde3af7635640b917cd
21120447 <haonhat2729@gmail.com>
2024-08-04 18:21:23 +0700

    fix: run test (4)

--- cypress/e2e/test-01.cy.ts
+++ cypress/e2e/test-01.cy.ts
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
   it('Does not do much!', () => {
-    cy.visit('http://localhost:3000/testing-cypress/')
+    cy.visit('https://usnightowl.github.io/testing-cypress/')
 
     cy.wait(3000)
     cy.contains('Baitaptracnghiem')
