f1032055dc26645819dcececb554612101cea918
21120447 <haonhat2729@gmail.com>
2024-08-04 18:55:55 +0700

    fix: run test (8)

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    projectId: 'kjhvhq',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-  projectId: 'kjhvhq',
 });
